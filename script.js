bc1c4a507cce67ac642e3e02dbc1587bbb38a547
adamgothmann <adamgothmann@gmail.com>
2017-06-19 03:28:29 -0500

    fonts

--- script.js
+++ script.js
@@ -2,25 +2,18 @@
 angular.module('myApp', [])
   .controller('mainController', ['$scope', function($scope){
     $scope.subscribe = function(){
-      if($scope.name.length < 5) {
-        alert("name must be at least 5 characters long");
-      } else if($scope.email.length < 8) {
-        alert("email must be at least 8 characters long");
+      if($scope.name.length < 5){
+        alert("name must be 5 or more characters long");
+        return false;
+      } else if ($scope.email.length < 8){
+        alert("email must be 8 or more characters long");
+        return false;
       }
+
       console.log($scope.name, $scope.email);
 
-      if($scope.name.length >= 5 & $scope.email.length >= 8){
+      if($scope.name.length >= 5 && $scope.email.length >= 8){
         $scope.valid = true;
-      }
-    };
-
-    $scope.checkLength = function(){
-      console.log('checkLength');
-      if($scope.name.length === null || $scope.name.length < 5 || $scope.email.length === null ||$scope.email.length < 8) {
-        document.getElementsByName("submit")[0].disabled = true;
-      }
-      else if($scope.name.length >= 5 && $scope.email.length >= 8){
-        document.getElementsByName("submit")[0].disabled = false;
       }
     };
   }]);
